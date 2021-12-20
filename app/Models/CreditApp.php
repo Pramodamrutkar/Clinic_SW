af78b913a880c288cdd980a84d909fa7c6f20f5f
=Pramod <=pramoda@a5econsulting.net>
2021-12-20 11:32:02 +0530

    made changes to credit app

--- app/Models/CreditApp.php
+++ app/Models/CreditApp.php
@@ -49,6 +49,11 @@ class CreditApp extends Model
         $this->autosaved = $request['autosaved'];
         $this->attempts = $request['attempts'];
         $this->when_last_attempted = $request['when_last_attempted'];
+        $this->timeout_resets = $request['timeout_resets'];
+        $this->servicing_system_id = $request['servicing_system_id'];
+        $this->monthly_income = $request['monthly_income'];
+        $this->knockout_lenders = $request['knockout_lenders'];
+        $this->submission = $request['submission'];
         if($this->save()){
             return response([
                 'success' => 'true',
@@ -95,4 +100,39 @@ class CreditApp extends Model
             ],400);
         }
     }
+
+
+
+    // public function storeDataintoSFDC($creditAppId){
+    //     CreditApp::where('creditapp_uuid',$creditAppId)->first();
+
+    //     $url = "";
+    //     $content = json_encode(array("Name" => $name));
+    //     $curl = curl_init($url);
+    //     curl_setopt($curl, CURLOPT_HEADER, false);
+    //     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt(
+    //         $curl,
+    //         CURLOPT_HTTPHEADER,
+    //         array(
+    //             "Authorization: OAuth $access_token",
+    //             "Content-type: application/json"
+    //         )
+    //     );
+    //     curl_setopt($curl, CURLOPT_POST, true);
+    //     curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+    //     $json_response = curl_exec($curl);
+    //     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    //     if ($status != 201) {
+    //         die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+    //     }
+    //     echo "HTTP status $status creating account";
+    //     curl_close($curl);
+    //     $response = json_decode($json_response, true);
+    //     $id = $response["id"];
+    //     echo "New record id $id";
+    //     return $id;
+    // }
+
+    
 }
